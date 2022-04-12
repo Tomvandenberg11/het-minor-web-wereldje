05b20bc534e23c9145e3ab3ccd5539c54a3a195e
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-04-12 12:27:36 +0200

    adding editable project selector

--- app.js
+++ app.js
@@ -15,10 +15,37 @@ app.set('views', 'views')
 
 app.use(express.static('static'))
 
+const readableProject = (project) => {
+  if (project === 'web-app-from-scratch-2122') {
+    project = 'Web apps from scratch'
+  }
+  if (project === 'css-to-the-rescue-2122') {
+    project = 'CSS to the rescue'
+  }
+  if (project === 'project-1-2122') {
+    project = 'Project OBA'
+  }
+  if (project === 'progressive-web-apps-2122') {
+    project = 'Progressive web apps'
+  }
+  if (project === 'browser-technologies-2122') {
+    project = 'Browser Technologies'
+  }
+  if (project === 'project-2-2122') {
+    project = 'Project GitHub'
+  }
+  return project
+}
+
 app.get("/", (req, res) => {
+  const formProject = req.query.project
+  const project = formProject ? formProject : 'web-app-from-scratch-2122'
+
+  console.log(project)
+
   graphqlAuth(`query {
   repositoryOwner(login: "cmda-minor-web") {
-    repository(name: "web-app-from-scratch-2122") {
+    repository(name: "${project}") {
       forks(first: 60) {
         edges {
           node {
@@ -38,6 +65,7 @@ app.get("/", (req, res) => {
 }`).then((data) => {
     res.render('index', {
       projects: data.repositoryOwner.repository.forks.edges,
+      currentProject: readableProject(project)
     })
   })
 })
